6149861640b9b952f82cf70b16e89e721f0c114c
david <davidsanchezdiaz72@gmail.com>
2023-12-26 11:51:58 +0100

    Función para mostrar el final del juego y el resultado

--- tres_en_raya_dom/app.js
+++ tres_en_raya_dom/app.js
@@ -3,7 +3,7 @@ const gameBoard = document.querySelector('.game_board');
 const messageTurn = document.querySelector('.game_turn');
 const endGame =document.querySelector('.endgame');
 const endGameResult = document.querySelector('.endgame_result');
-const buttonResert = document.querySelector('.endgame_button');
+//const buttonResert = document.querySelector('.endgame_button');
 
 
 // Variables de control del estado del juego
@@ -15,13 +15,15 @@ let player = {
     o:'circle'
 }
 
+createBoard();
+
 const winningPosition = [
-    [0,1,2], [3,4,5], [6,7,8], [0,3,6]
+    [0,1,2], [3,4,5], [6,7,8], [0,3,6],
     [1,4,7], [2,5,8], [0,4,8], [2,4,6]
 ]
 
 
-createBoard();
+
 
 // Función para crear el tablero de juego
 function createBoard() {
@@ -49,12 +51,13 @@ function handleGame(e) {
     
     turn++;
     drawShape(currentCell, currentTurn);
-    changeTurn();
+
+    
     if (checkWinner(currentTurn)) {
         return;
     }
 
-    
+    changeTurn();
     checkWinner(currentTurn);
 
     
@@ -74,34 +77,39 @@ function changeTurn() {
 function checkWinner(currentPlayer) {
     const cells = document.querySelectorAll('.cell');
 
-        // Verificar combinaciones ganadoras
-        const winner = winningPosition.some(array => {
-            
-            console.log(array);
-    
-
-            // Verificar si la combinación actual pertenecen al jugador
-            return array.every(position => {
-
-                return cells[position].classList.contains
-                (currentPlayer);
-            })
-            
-        })    
-
-        if (!winner) {
-            return;
-        }
-        
-        showEndGame(true);
-        return true;
-    };
-
-    function showEndGame(winner){
-
+    // Verificar que 'winningPosition' esté definido y sea un array
+    if (!winningPosition || !Array.isArray(winningPosition)) {
+        console.error('winningPosition no está definido correctamente.');
+        return;
     }
 
+    // Verificar combinaciones ganadoras
+    const winner = winningPosition.some(array => {
+        console.log(array);
 
-    
+        // Verificar si todas las posiciones en 'array' son válidas y pertenecen al jugador actual
+        return array.every(position => {
+            const cell = cells[position];
+            return cell && cell.classList.contains(currentPlayer);
+        });
+    });
 
+    if (winner) {
+        showEndGame(true);
+        return true;
+    }
+
+    return false;
+}
+
+// Función para mostrar el final del juego y el resultado
+    function showEndGame(winner){
+        endGame.classList.add('show')
+
+        // Actualiza el contenido del resultado final
+        if (winner) {
+            endGameResult.textContent = `${isTurnX ? 'x' : 'o' }
+            Ha ganado el juego!`;
+        }
+    }
 
