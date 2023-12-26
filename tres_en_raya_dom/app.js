95dd455ee2678bd6affce065fe0cea0ce83f6b29
david <davidsanchezdiaz72@gmail.com>
2023-12-26 12:14:42 +0100

    Volver a reiniciar el juego

--- tres_en_raya_dom/app.js
+++ tres_en_raya_dom/app.js
@@ -3,7 +3,7 @@ const gameBoard = document.querySelector('.game_board');
 const messageTurn = document.querySelector('.game_turn');
 const endGame =document.querySelector('.endgame');
 const endGameResult = document.querySelector('.endgame_result');
-//const buttonResert = document.querySelector('.endgame_button');
+const buttonResert = document.querySelector('.endgame_button');
 
 
 // Variables de control del estado del juego
@@ -15,13 +15,19 @@ let player = {
     o:'circle'
 }
 
-createBoard();
-
 const winningPosition = [
     [0,1,2], [3,4,5], [6,7,8], [0,3,6],
     [1,4,7], [2,5,8], [0,4,8], [2,4,6]
 ]
+startGame();
 
+function startGame() {
+    createBoard();
+    messageTurn.textContent = isTurnX ? 'X' : 'O';
+    isTurnX = true;
+    turn = 0;
+    endGame.classList.remove('show');
+}
 
 
 
@@ -57,10 +63,12 @@ function handleGame(e) {
         return;
     }
 
-    changeTurn();
-    checkWinner(currentTurn);
+    if (turn === maxTurn) {
+        showEndGame(false);
+    }
 
-    
+    changeTurn();
+
 }
 
 // Dibujar el símbolo del jugador
@@ -85,11 +93,11 @@ function checkWinner(currentPlayer) {
 
     // Verificar combinaciones ganadoras
     const winner = winningPosition.some(array => {
-        console.log(array);
 
         // Verificar si todas las posiciones en 'array' son válidas y pertenecen al jugador actual
         return array.every(position => {
             const cell = cells[position];
+
             return cell && cell.classList.contains(currentPlayer);
         });
     });
@@ -108,8 +116,13 @@ function checkWinner(currentPlayer) {
 
         // Actualiza el contenido del resultado final
         if (winner) {
-            endGameResult.textContent = `${isTurnX ? 'x' : 'o' }
+            endGameResult.textContent = `¡${isTurnX ? 'x' : 'o' }
             Ha ganado el juego!`;
+        }else {
+            endGameResult.textContent = 'El resultado del juega ha sido un empate';
+            
         }
     }
+
+    buttonResert.addEventListener('click', showEndGame);
 
